e636baaeb8bb086822e14f09475c616e7be4ed29
ThBarnabe <thiagov.barnabe@gmail.com>
2021-01-08 22:20:00 -0300

    Adiciona Relação entre a tabela clientes e contato

---
+++ src/SGC.Infrastructure/Migrations/20210109003704_AdicionaRelacaoEntreTabelas.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SGC.Infrastructure.Migrations
+{
+    public partial class AdicionaRelacaoEntreTabelas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
